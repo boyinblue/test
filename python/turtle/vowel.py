7d913aedc2a2b9036d5ba2be7fc722613d0650e8
boyinblue <boyinblue@naver.com>
2022-03-21 16:24:49 +0900

    add turtle example

--- python/turtle/vowel.py
+++ python/turtle/vowel.py
@@ -1,4 +1,5 @@
-from turtle import *
+import turtle
+import time
 
 def draw_a():
     turtle.write("a", True)
@@ -16,8 +17,18 @@ def draw_u():
     turtle.write("u", True)
 
 def start():
-    setup(600, 400)
-    width(9)
-    speed(0)
-    penup()
-    goto(-220, -30)
+    cnt = 0
+    turtle.setup(width=600, height=400)
+    turtle.width(9)
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(-220, -30)
+    while cnt < 10:
+        draw_e()
+        draw_o()
+        draw_u()
+        time.sleep(1)
+        cnt = cnt + 1
+
+if __name__ == '__main__':
+    start()
