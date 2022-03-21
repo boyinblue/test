e687eb6d65476013b2a2a2f5757855153e44a122
boyinblue <boyinblue@naver.com>
2022-03-21 15:41:37 +0900

    add turtle

---
+++ python/turtle/random_vowels.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+def draw_a():
+
+def draw_e():
+
+def draw_i():
+
+def draw_o():
+
+def draw_u():
+
+def start():
+    setup(600, 400)
+    width(9)
+    speed(0)
+    penup()
+    goto(-220, -30)
---
+++ python/turtle/vowel.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+def draw_a():
+    turtle.write("a", True)
+
+def draw_e():
+    turtle.write("e", True)
+
+def draw_i():
+    turtle.write("i", True)
+
+def draw_o():
+    turtle.write("o", True)
+
+def draw_u():
+    turtle.write("u", True)
+
+def start():
+    setup(600, 400)
+    width(9)
+    speed(0)
+    penup()
+    goto(-220, -30)
