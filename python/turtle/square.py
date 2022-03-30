050c977ebc2da21345d4b55360050c68ece660c6
boyinblue <boyinblue@naver.com>
2022-03-31 07:20:53 +0900

    square.py

---
+++ python/turtle/square.py
@@ -0,0 +1,12 @@
+import turtle
+import time
+
+turtle.setup(width=600, height=400)
+turtle.width(9)
+turtle.ipeed(1)
+turtle.penup()
+turtle.goto(-220, -30)
+turtle.forward(100)
+turtle.left(100)
+turtle.backward(100)
+turtle.right(100)
