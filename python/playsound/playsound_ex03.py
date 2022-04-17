2c44571bc29942aec596a6f49f119069130e986a
boyinblue <boyinblue@naver.com>
2022-04-18 08:13:30 +0900

    update

--- python/playsound/playsound_ex03.py
+++ python/playsound/playsound_ex03.py
@@ -1,5 +1,25 @@
 import playsound
-import time
+import os
+import tempfile
+
+temp_file = None
+
+def stop_mp3():
+    global temp_file
+
+    if not temp_file
+        return
+
+    os.remove(temp_file)
+
+def play_mp3(filename):
+    global temp_file
+
+    fd, filename = tempfile.mkstemp()
+
+    stop_mp3()
+
+    
 
 playsound.playsound("sample.mp3", block=False)
 time.sleep(5)
